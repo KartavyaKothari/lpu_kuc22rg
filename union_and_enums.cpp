f05adff8e5f31d5ff84c3ef0fbafb9c600bad7ae
Kartavya kothari <kartavya.kothari24@gmail.com>
2021-01-28 09:48:09 +0530

    Added enus and unions

---
+++ union_and_enums.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+enum Weekday {
+    Mon = 100,
+    Tues,
+    Wed,
+    Thurs,
+    Fri,
+    Sat,
+    Sun
+};
+
+// int Mon = 0;
+// int Tues = 1;
+
+int main(){
+    // union StudentRecord{
+    //     int var1;
+    //     char gender;
+    // } temp;
+
+    // temp.gender = 'A';
+    // temp.var1 = 12;
+
+    // cout<<temp.gender;
+
+    int day = 101;
+
+    cout<<Sat;
+    if(day<Sat) cout<<"It is not saturday yet";
+
+    return 0;
+}
