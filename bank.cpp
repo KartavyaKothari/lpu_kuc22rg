da4e765889e36a5517e528180c353aa9964c96cc
Kartavya kothari <kartavya.kothari24@gmail.com>
2021-02-04 09:54:59 +0530

    Added bank .cpp code

--- bank.cpp
+++ bank.cpp
@@ -1,11 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+struct transaction{
+    bool is_deposit;
+    int amt;
+};
+
 class Account{
     int acc_num;
     string pass;
     int balance;
     int num_trasac;
+
+    struct transaction history[100];
+    int last_transac;
 
     bool check_pass(){
         string p;
@@ -36,6 +44,7 @@ class Account{
         balance = 100;
         num_trasac = 0;
         pass = "pass@123";
+        last_transac = -1;
     }
 
     void check_balance(){
@@ -56,6 +65,9 @@ class Account{
         // if(is_transaction_limit_reached()){
         //     return;
         // }
+
+        last_transac++;
+        history[last_transac] = {false,amt};
         
         balance = balance - amt;
         cout<<"Withdraw successfull, balance left is "<<balance<<" rupees"<<endl;
@@ -69,8 +81,23 @@ class Account{
         //     return;
         // }
 
+        last_transac++;
+        history[last_transac] = {true,amt};
+
         balance = balance + amt;
         cout<<"Adding money successfull, balance is "<<balance<<" rupees"<<endl;
+    }
+
+    void update_passbook(){
+        cout<<"***********Transaction history*************"<<endl;
+        for(int i = 0 ; i <= last_transac ; i++){
+            if(history[i].is_deposit){
+                cout<<"Deposited "<<history[i].amt<<" rupees"<<endl;
+            }else{
+                cout<<"Withdrew "<<history[i].amt<<" rupees"<<endl;
+            }
+        }
+        cout<<"************************"<<endl;
     }
 };
 
@@ -104,6 +131,7 @@ int main(){
     p1.withdraw_money(57);
     p1.add_money(100);
     p1.withdraw_money(57);
+    p1.update_passbook();
 
     return 0;
 }
