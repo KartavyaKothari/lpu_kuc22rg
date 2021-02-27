e4bacd57d2020465b3130c0a29f6706408190462
Kartavya kothari <kartavya.kothari24@gmail.com>
2021-02-27 14:40:02 +0530

    Updated slides

---
+++ week5/arrays.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr[] = {1,2,3,4,5};
+    // int arr[5] = {1,2,3,4,5};
+
+    for(int i = 0 ; i < 5 ; i++){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
